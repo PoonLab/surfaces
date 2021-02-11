b327b4a9847a64c75225543ff378e7bef690f1e1
Sarehchimeh <63608535+Sarehchimeh@users.noreply.github.com>
2021-02-11 10:48:01 -0500

    Update prune_CDS2.py

--- prune_CDS2.py
+++ prune_CDS2.py
@@ -41,24 +41,66 @@ Welcome to Ubuntu 18.04.5 LTS (GNU/Linux 4.15.0-132-generic x86_64)
 New release '20.04.2 LTS' available.
 Run 'do-release-upgrade' to upgrade to it.
 
-*** System restart required ***
-Last login: Wed Feb 10 20:36:38 2021 from 99.251.234.133
-sareh@Erasmas:~$ cd script/
-sareh@Erasmas:~/script$ ls
-fasttree_iterator.sh       prune_CDS.py           prunetree_iterator.sh
-gotoh2                     pruned_accn.py         prunetree.py
-performance_prune_CDS.txt  prunetree_iterator.py  test.sh
-sareh@Erasmas:~/script$ nano prune_CDS.py 
+                              
 
-  GNU nano 2.9.3                     prune_CDS.py                               
+#! /usr/bin/python3
+#prune a fasta file from (Data/CDS nucleotide sequence)
+# to keep only the sequence entries of the accn sequences in Data/pruned_accn
+# input Data/CDS nucleotide sequence & Data/pruned_accn directories
 
+import os
+import re
+from Bio import SeqIO
+
+def get_accn(file):
+    """
+    Stream through txt file retrieve accns into a list
+    """
+    lst_accns = []
+    with open(file, 'r') as f:
+        for line in f:
+            line = line.strip('\n')
+            lst_accns.append(line)
+    a = tuple(lst_accns)
+    return(a)
+
+# if "accn" in tuple:
+
+def main():
+    accn_directory = '/home/sareh/data/pruned_accns'
+    cds_directory = '/home/sareh/data/CDS_nucleotide_seq'
+    for filename1 in os.listdir(accn_directory):
+        print('filename1 is '+ filename1)
+        #filename1='pruned_accn_NC_007605'
+        accn1=filename1[12:]
+        print('accn1 ' + accn1)
+        path1 = '{}/{}'.format(accn_directory, filename1)
+        path2 = '{}/{}.txt'.format(cds_directory, accn1)
+        print(path2)
+        # filename2='NC_001526.fasta'
+        accessions= get_accn(path1)
+        print(accessions)
+        #all accessions in a tuple
+        with open('/home/sareh/data/Pruned_CDS/Pruned_CDS_{}'.format(accn1),'w') as o$
+            pruned_record_count = 0
+            record_count = 0
+            with open(path2,'r') as f2:
+                for record in SeqIO.parse(path2, 'fasta'):
+                    record_count += 1
+                    id = []
+                    a = record.id
+                    x = re.findall('[A-Z]{1,3}[0-9]{5,6}.[0-9]', a)
+                    # accns in a list
                     for i in x:
                         if i in accessions:
                             pruned_record_count +=1
                             SeqIO.write(record,out_file,'fasta')
         print('record count: '+ str(record_count))
         print('pruned record count: '+ str(pruned_record_count))
-                                    # write the next line into a file (record s$
+                                    # write the next line into a file (record sequenc$
 
 if __name__ == "__main__":
     main()
+
+
+
