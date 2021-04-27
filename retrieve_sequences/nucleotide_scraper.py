1742619ad0fd356b187c8847874d4ae4b604cb12
Sarehchimeh <63608535+Sarehchimeh@users.noreply.github.com>
2021-04-27 16:26:34 -0400

    Update nucleotide_scraper.py
    Modified to retrieve nucleotide sequences of the pruned accns (100) 
    --accndir is a txt file with accns in each line

--- retrieve_sequences/nucleotide_scraper.py
+++ retrieve_sequences/nucleotide_scraper.py
@@ -31,10 +31,10 @@ def pase_filtered_neighbour_table(table):
     """
     lst = []
     with open(table, 'r') as f:
-        print(table)
-        for line in f:
-            name = line.split("\n")
-            lst.append(name[1])
+    print(table)
+    for line in f:
+        name = line.split("\n")
+        lst.append(name[1])
     return (lst)
 
 def retrieve_gid(accn):
@@ -63,23 +63,23 @@ def main():
     file_paths = glob(os.path.join(args.accndir, 'pruned_accn_*'))
     for path in file_paths:
         filename = os.path.basename(path)
-	print("filename is ' + filename)
-	print("path is ' + path)
-    	values = pase_filtered_neighbour_table(path)
+        print("filename is " + filename)
+        print("path is " + path)
+        values = pase_filtered_neighbour_table(path)
         accn = filename.replace('pruned_accn_', '')  # [12:]
         with open('{}/Pruned_nuc_{}'.format(args.outdir, accn.'w') as outfile:
-        	for accn in values:
-            		gid = retrieve_gid(accn)
-            		if gid is None:
-                		print('Warning, failed to retrieve gid for {}'.format(accn))
-                		continue
-                		print(accn + ' ' + gid)
-            		sleep(2)  # avoid spamming the server
-            		record = retrieve_record(gid)
-            		name = record.annotations['organism']
-            		seq = record.seq
-            		outfile.write('>{},{},\n{}\n'.format(name, accn, seq)
-        	sleep(2)
+            for accn in values:
+                gid = retrieve_gid(accn)
+                if gid is None:
+                    print('Warning, failed to retrieve gid for {}'.format(accn))
+                    continue
+                    print(accn + ' ' + gid)
+                    sleep(2)  # avoid spamming the server
+                    record = retrieve_record(gid)
+                    name = record.annotations['organism']
+                    seq = record.seq
+                    outfile.write('>{},{},\n{}\n'.format(name, accn, seq)
+            sleep(2)
 
 if __name__ == "__main__":
     main()
