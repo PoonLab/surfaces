3a56c30bc957b71e200e644339c1a6c12380ee78
Sareh Bagherichimeh <63608535+Sarehchimeh@users.noreply.github.com>
2021-08-26 12:37:31 -0400

    Create json_heatmap.r
    Read jason file and save jpg of heatmap of posterior probability 
    json_path <- args[1]
    outpath <- args[2]

---
+++ fubar/json_heatmap.r
@@ -0,0 +1,37 @@
+# Read jason file and save jpg of heatmap of posterior probability 
+
+#this didn't work: Error in file(con, "r") : cannot open the connection
+args = commandArgs(trailingOnly=TRUE)
+
+json_path <- args[1]
+outpath <- args[2]
+
+#title
+name <- gsub("/home/sareh/surfaces/find_cds/working2/FUBAR/cutter_cds_","",json_path)
+name <- gsub(".FUBAR.json","",name)
+
+# Load the package required to read JSON files.
+library("rjson")
+library("ggplot2")
+
+# Give the input file name to the function.
+result <- fromJSON(file = json_path)
+
+#grid as df
+grid.matix <- matrix(unlist(result$grid),ncol=3,byrow=TRUE)
+grid.df <- as.data.frame(grid.matix)
+colnames(grid.df) <- c("dN","dS","posterior")
+
+#as factor 
+grid.factor <- grid.df
+grid.factor$dN<- as.factor(round(grid.factor$dN,digits = 2))
+grid.factor$dS <- as.factor(round(grid.factor$dS,digits = 2))
+
+# Heatmap 
+png(file=outpath,width=600, height=600)
+ggplot(grid.factor, aes(grid.factor$dN, grid.factor$dS, fill= grid.factor$posterior)) +
+geom_tile() + theme(axis.text.x = element_text(angle = 90)) +
+  xlab("Nonsynonymous rate") + ylab("Synonymous rate") + labs(fill = "Posterior Mean") +
+ggtitle(name) + theme(plot.title = element_text(lineheight=2, face="bold", size = 10, hjust = 0.5))
+dev.off()
+
