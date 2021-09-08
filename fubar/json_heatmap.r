fed8d78159c1ec7b3678160cd277eb4117840fe5
Sareh Bagherichimeh <63608535+Sarehchimeh@users.noreply.github.com>
2021-09-08 14:41:48 -0400

    Update json_heatmap.r
    switched the dN and dS values on the x & y axsis

--- fubar/json_heatmap.r
+++ fubar/json_heatmap.r
@@ -1,3 +1,4 @@
+#!/usr/bin/Rscript
 # Read jason file and save jpg of heatmap of posterior probability 
 
 #this didn't work: Error in file(con, "r") : cannot open the connection
@@ -7,8 +8,8 @@ json_path <- args[1]
 outpath <- args[2]
 
 #title
-name <- gsub("/home/sareh/surfaces/find_cds/working2/FUBAR/cutter_cds_","",json_path)
-name <- gsub(".FUBAR.json","",name)
+name <- gsub("/home/sareh/re-do/FUBAR/fubar100/json/","",json_path)
+name <- gsub("clean.FUBAR.json","",name)
 
 # Load the package required to read JSON files.
 library("rjson")
@@ -29,9 +30,9 @@ grid.factor$dS <- as.factor(round(grid.factor$dS,digits = 2))
 
 # Heatmap 
 png(file=outpath,width=600, height=600)
-ggplot(grid.factor, aes(grid.factor$dN, grid.factor$dS, fill= grid.factor$posterior)) +
+ggplot(grid.factor, aes(grid.factor$dS, grid.factor$dN, fill= grid.factor$posterior)) +
 geom_tile() + theme(axis.text.x = element_text(angle = 90)) +
-  xlab("Nonsynonymous rate") + ylab("Synonymous rate") + labs(fill = "Posterior Mean") +
+  xlab("Synonymous rate") + ylab("Nonsynonymous rate") + labs(fill = "Posterior Mean") +
 ggtitle(name) + theme(plot.title = element_text(lineheight=2, face="bold", size = 10, hjust = 0.5))
 dev.off()
 
