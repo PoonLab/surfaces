a209b7a2dc11f75e5e65b76b8815a56a24283203
Sarehchimeh <sareh.bagherichimeh@gmail.com>
2021-09-08 22:48:32 -0400

    treelength pruning

--- downsizing/prunetree.py
+++ downsizing/prunetree.py
@@ -1,22 +1,29 @@
-#! /usr/bin/bash
+#! /usr/bin/python
+
 import sys
 from Bio import Phylo
 
-tr = Phylo.read(sys.argv[1], 'newick')
-target = int(sys.argv[2])
+phy = Phylo.read(sys.argv[1], 'newick')
+thrshld = float(sys.argv[2])
+outpath = sys.argv[3]
 
-tips = tr.get_terminals()
+tips = phy.get_terminals()
 
-if target >= len(tips):
-    print("Tree already has fewer tips than target.")
+tree_length = phy.total_branch_length()
+
+if thrshld > tree_length:
+    print("{} Treelength is less than threshold".format(sys.argv[1]))
     sys.exit()
 
-while len(tips) > target:
+while tree_length >= thrshld:
     # find shortest tip
     lengths = [(tip.branch_length, tip.name) for tip in tips]
     lengths.sort()
     min_length, min_tip = lengths[0]
-    tr.prune(min_tip)
-    tips = tr.get_terminals()
-    
-Phylo.write(tr, sys.stdout, 'newick')
+    phy.prune(min_tip)
+    tree_length = phy.total_branch_length()
+    tips = phy.get_terminals()
+
+
+print("{},{},{}\n".format(sys.argv[1],phy.count_terminals(),tree_length))
+Phylo.write(phy,outpath, 'newick')
