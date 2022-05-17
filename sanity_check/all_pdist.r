e9016d5ec1767c7c4fa0d66e1d5b4793fcd1a17e
Sareh Bagherichimeh <63608535+Sarehchimeh@users.noreply.github.com>
2022-05-17 17:29:44 -0400

    Create all_pdist.r
    Takes in the csv files created by the cut_cds script for each cds
    takes the average of p-dist of every sequences for a cds and writes it out in a line of a csv file

---
+++ sanity_check/all_pdist.r
@@ -0,0 +1,22 @@
+#!/usr/bin/R
+
+# output csv with mean of each file (csv)  
+
+args = commandArgs(trailingOnly=TRUE)
+csv_path <- args[1]
+outpath <- args[2] 
+
+name <- gsub(".csv","", csv_path))
+df <- read.csv(path)
+
+p_mean = round(mean(df$P), digits = 3)
+p_noovlp = round(mean(df$P_noovlp), digits = 3)
+range_p = round(range(df$P), digits = 3)
+ovlp_len = median(df$original_refgene) - median(df$len_noovlp)
+
+row <- paste(name, p_mean, p_noovlp, range_p[1], range_p[2], range_p[2]-range_p[1], ovlp_len)
+
+#Writing out the CSV (append)
+out.file<- file("./all_pdist.csv", "a")
+cat(row, file=out.file, append=TRUE, sep = "\n")
+close(out.file)
