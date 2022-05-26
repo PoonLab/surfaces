f4defb8a5738ee3cd1fdbad9093fe344ffc4cef7
Sareh Bagherichimeh <63608535+Sarehchimeh@users.noreply.github.com>
2022-05-26 11:05:58 -0400

    All posterior prob in one file
    Reads a JSON file to extract all posterior probabilities (400) 
    appends it to the output file (no header) 
    
    Can get all posterior prob in one file 
    for f in NC_*; do Rscript all_posterior_prob.r $f all_posterior_prob.csv; done 
    Used to get the range of all posterior probabilities to determine the consistent range for heatmaps

---
+++ fubar/all_posterior_prob.r
@@ -0,0 +1,28 @@
+#!/usr/bin Rscript --vanilla
+
+# Load the package required to read JSON files.
+library("rjson")
+library("ggplot2")
+
+# input arguments 
+args = commandArgs(trailingOnly=TRUE)
+json_path <- args[1]
+outpath <- args[2]
+
+# Give the input file name to the function.
+result <- fromJSON(file = json_path)
+
+#grid as df
+grid.matix <- matrix(unlist(result$grid),ncol=3,byrow=TRUE)
+grid.df <- as.data.frame(grid.matix)
+colnames(grid.df) <- c("dN","dS","posterior")
+
+#as factor 
+grid.factor <- grid.df
+grid.factor$dN<- as.factor(round(grid.factor$dN,digits = 2))
+grid.factor$dS <- as.factor(round(grid.factor$dS,digits = 2))
+
+grid.factor$log <- log10(grid.factor$posterior)
+
+# writing out the data frame (appending each to the bottom)
+write.table(grid.factor, outpath, sep = ",", append = T)
