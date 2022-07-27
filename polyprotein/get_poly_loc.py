a87df29b4f9f4ffd8b27cecbff1d2166c8b83559
Sareh Bagherichimeh <63608535+Sarehchimeh@users.noreply.github.com>
2022-07-27 00:36:31 -0400

    Update get_poly_loc.py
    identifies segmented virus based on GenBank record

--- polyprotein/get_poly_loc.py
+++ polyprotein/get_poly_loc.py
@@ -1,38 +1,62 @@
+
 from Bio import SeqIO
 import os
 import argparse
+import csv
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--out', type=str,
-                       help='text files of genbank record')
+    parser.add_argument('--loc', type=str,
+                       help='csv with location of polyprotiens')
+    parser.add_argument('--csv', type=str,
+                       help='csv identifying segmented and polyproteins')
     parser.add_argument('--dir', type=str,
-                       help='directory')
+                       help='directory of genbank record files')
     return parser.parse_args()
-
-#dir = "/home/sareh/2020/polyprotein/gb_record/"
-#outpath = "/home/sareh/2020/polyprotein/poly_loc2.csv"
 
 args = parse_args()
 
 for file in os.listdir(args.dir):
-  file_path = os.path.join(args.dir,file)
+    file_path = os.path.join(args.dir,file)
+    record = SeqIO.read(file_path, "gb")
 
-  record = SeqIO.read(file_path, "gb")
+    line = []
+    d = {}
+    
+    line.append(file)
+    d["id"] = file
 
-  count = 0
-  for feature in record.features:
-     if feature.type == 'mat_peptide':
-        count += 1
-        try:
-           loc = feature.location
-           prot_accn = feature.qualifiers['protein_id'][0]
-           #print(feature)
-        except:
-           print("issue")
-           print(record.id)
-           print(feature)
-        if count > 1:
-           outfile = open(args.out, 'a')
-           line = "{},{},{}\n".format(record.id, prot_accn, loc)
-           outfile.write(line)
+    for feature in record.features:
+        # check if it's a segmented virus
+        if feature.type == "source" and 'segment' in feature.qualifiers:
+            line.append("seg")
+            d["seg"] = "TRUE"
+            #print(feature.qualifiers['segment'])
+        else:
+            line.append("nonseg")
+            d["seg"] = "FALSE"
+
+    for feature in record.features:
+        # check is it's a polyprotein
+        if feature.type == 'mat_peptide':
+            d["poly"] = "TRUE"
+            
+            try:
+                loc = feature.location
+                prot_accn = feature.qualifiers['protein_id'][0]
+                line = "{},{},{}\n".format(record.id, prot_accn, loc)
+                loc_handle = open(args.loc, 'a')
+                loc_handle.write(line)
+
+            except:
+                # skip viruses with missing record ID 
+                print("issue: " + record.id)
+        else: 
+            d["poly"] = "FALSE"
+
+    #write out the dictionary 
+    l = ("{},{},{}\n".format(d["id"], d["seg"], d["poly"]))
+    csv_handle = open(args.csv, "a")
+    csv_handle.write(l)
+
+print(d)
