ae8805f61c5809d00d9b52753ef2b1d051c532e0
Sareh Bagherichimeh <63608535+Sarehchimeh@users.noreply.github.com>
2022-08-15 23:36:18 -0400

    changed ft to now capture the output and write it out

--- downsizing/downsizing.py
+++ downsizing/downsizing.py
@@ -7,10 +7,9 @@ output : pruned_nbr_genome & pruned_nbr_accn
 3. mini_out_dir
 4. ft_out_dir
 5. pruned_ft_dir
-6. pruned_fa_dir 
+6. pruned_fa_dir
 7. pruned_nbr_accn.txt
 """
-
 
 #!/usr/bin python3
 
@@ -21,20 +20,32 @@ import os
 import re
 import argparse
 from Bio import SeqIO
+import time
 
 mm2bin = "/home/sareh/2020/scripts/downsizing/minimap2.py"
-nbr_dir = "/home/sareh/2020/test/nbr_dir"
+
+# INPUT
+nbr_dir = "/home/sareh/2020/july/all_nbr_genome"
 #nbr_dir = "/home/sareh/2020/sequences/nuc_genome/"
-ref_dir = "/home/sareh/2020/test/ref_dir"
+ref_dir = "/home/sareh/2020/july/poly_ref_genome"
 #ref_dir = "/home/sareh/2020/sequences/ref_nuc_genome/"
-mini_out_dir = "/home/sareh/2020/test/msa"
+# OUTPUT
+base_dir = "/home/sareh/2020/july"
+mini_out_dir = "msa_nbr_genome"
 #mini_out_dir = "/home/sareh/2020/downsizing/all_genome_msa/"
-ft_out_dir = "/home/sareh/2020/test/fasttree"
+ft_out_dir = "tr_nbr_genome"
 #ft_out_dir = "/home/sareh/2020/downsizing/all_genome_fasttree/"
-pruned_ft_dir = "/home/sareh/2020/test/pruned_fasttree"
+pruned_ft_dir = "pruned_tr_nbr_genome"
 #pruned_ft_dir = "/home/sareh/2020/downsizing/pruned_fasttree"
-pruned_fa_dir = "/home/sareh/2020/test/pruned_fasta"
+pruned_fa_dir = "pruned_nbr_genome"
 #pruned_fa_dir = "/home/sareh/2020/downsizing/pruned_ncbi_genome"
+pruned_accn_dir = "pruned_nbr_accn"
+
+# mpi
+from mpi4py import MPI
+the_world = MPI.COMM_WORLD
+my_number = the_world.Get_rank()
+total_number = the_world.Get_size()
 
 def convert_fasta (handle):
     """
@@ -57,7 +68,7 @@ def convert_fasta (handle):
     result.append([h,sequence]) # handle last entry
     return result
 
-def prune (tr_path, target): 
+def prune (tr_path, target):
 
     tr = Phylo.read(tr_path, 'newick')
 
@@ -74,7 +85,7 @@ def prune (tr_path, target):
         min_length, min_tip = lengths[0]
         tr.prune(min_tip)
         tips = tr.get_terminals()
-    
+
     return(tr)
 
 def accn_from_tre (file):
@@ -93,42 +104,53 @@ def accn_from_tre (file):
 
 
 def main():
-
+    count = 0
     for ref_file in os.listdir(ref_dir):
+        count += 1 # mpi
+        if count % total_number != my_number:
+                continue
+        #file paths
+        ref_path = os.path.join(base_dir, ref_dir, ref_file)
+        nbr_path = os.path.join(base_dir, nbr_dir, ref_file) # NC_009996.csv
+        #accn_fa = "{}.fa".format(ref_file)
+        mini_out_path = os.path.join(base_dir, mini_out_dir, ref_file)
+        ft_out_path = os.path.join(base_dir, ft_out_dir, ref_file)
+        pruned_ft_path = os.path.join(base_dir, pruned_ft_dir, ref_file)
+        pruned_fa_path = os.path.join(base_dir, pruned_fa_dir, ref_file)
+        pruned_accn_path = os.path.join(base_dir, pruned_accn_dir, ref_file)
 
-        ref_path = os.path.join(ref_dir, ref_file) #WHY?
-        nbr_path = os.path.join(nbr_dir,ref_file) #"/home/sareh/2020/sequences/test/NC_001802.txt"
-        accn_fa = "{}.fa".format(ref_file)
-        mini_out_path = os.path.join(mini_out_dir, accn_fa) #"/home/sareh/2020/sequences/test/minimap_out.fa"
-        ft_out_path = os.path.join(ft_out_dir, ref_file)
-        pruned_ft_path = os.path.join(pruned_ft_dir, ref_file)
-        pruned_fa_path = os.path.join(pruned_fa_dir, ref_file)
- 
         print(mini_out_path)
         nthread = 5
 
         #1. python3 minimap2.py -o minimap_alignment.fasta --ref ref.hiv.test.fa NC_001802.txt -f -a
-        cmd_minimap = ['python3', mm2bin, '-o',str(mini_out_path), str(nbr_path), '--ref', str(ref_path), '-f', '-a', '-t', str(nthread)]
+        cmd_minimap = ['python3', mm2bin, '-o', str(mini_out_path), '--ref', str(ref_path), str(nbr_path), '-f', '-a', '-t', str(nthread)]
         subprocess.call(cmd_minimap)
+        print(mini_out_path)
 
         #2. fasttree -nt -gtr -gamma minimap_alignment.fasta > fasttree.tre
-        print(mini_out_path)
-        ###DOSEN"T WORK
-        cmd_fasttree = ['fasttree', '-nt', '-gtr', '-gamma', str(mini_out_path), '>', str(ft_out_path)]
-        subprocess.call(cmd_fasttree)
+        cmd_fasttree = ['fasttree', '-nt', '-gtr', '-gamma','-quiet', str(mini_out_path)]
+        ft = subprocess.run(cmd_fasttree, stdout=subprocess.PIPE)
+
+        #writing it out
+        ft_handle = open(ft_out_path, "w")
+        ftout = str(ft.stdout)[2:-3] #remove: b' #remove: '\n
+        ft_handle.write(ftout)
         print("fasttree ran!!")
+        sleep(2)
 
         #3. fasttree.tre > pruned_fasttree.tre
         pruned_tr = prune(ft_out_path, 100)
         Phylo.write(pruned_tr, pruned_ft_path, 'newick')
+        sleep(2)
 
         #4. pruned_fasttree.tre > pruned_ncbi_nuc_genome
-
         pruned_accns = tuple(accn_from_tre(pruned_ft_path))
+        sleep(2)
 
+        # write out accns
         pruned_fa = open(pruned_fa_path,'w')
-        fasta_file = open(nbr_path, 'r') 
- 
+        fasta_file = open(nbr_path, 'r')
+
         for record in SeqIO.parse(fasta_file, 'fasta'):
             id = []
             x = re.findall('[A-Z]{1,3}[0-9]{5,6}.[0-9]', record.id)
