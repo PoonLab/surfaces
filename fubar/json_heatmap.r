ce16b5aa42233a94df0c2afa25bf358907d01fe4
Sareh Bagherichimeh <63608535+Sarehchimeh@users.noreply.github.com>
2022-11-02 11:18:41 -0400

    cleaned up, fixed x and y axsis

--- fubar/json_heatmap.r
+++ fubar/json_heatmap.r
@@ -1,37 +1,21 @@
 #!/usr/bin/R
-
-### INPUT 
-# json
-# midpoint =-2.06, limits=c(-3.80, -0.31))
-# limits
-# name_cleanup (gsub)
-
-### OUTPUT
-# heatmap.png
-# all_posterior-prob.csv
-# max.min.pearson_cor.csv
-
-### INFO
-# Read jason file and save jpg of heatmap of posterior probability with set range
-# old_ncbi: midpoint = -5.175, limits=c(-9.59, -0.76))
-# lin(april): midpoint = -2.12, limits=c(-3.86, -0.37))
-# ncbi(april): midpoint =-2.06, limits=c(-3.80, -0.31))
-
-args = commandArgs(trailingOnly=TRUE)
-json_path <- args[1]
-outpath <- args[2]
-
-# log transformed ranges
-mid_range = -2.2454638
-prob_range = c(-4.1639958,-0.3269318)
-
-#title
-#cutter_cds_NC_039223_YP_009513196.1.clean.FUBAR.json
-name <- gsub(".clean.FUBAR.json","",gsub("/home/sareh/new_cds/FUBAR/json/cutter_cds_","",json_path))
-
 # Load the package required to read JSON files.
 library("rjson")
 library("ggplot2")
+
+### INPUT 
+mid_range = -2.235
+prob_range = c(-4.1633,-0.3067)
+# log default : midpoint =-5.14635, limits=c(-9.5864, -0.7063))
+# log 10 : midpoint =-2.235, limits=c(-4.1633, -0.3067))
+
+# 2. name_cleanup (gsub)
+name <- gsub(".clean.FUBAR.json","",json_path)
+
+# 3. file in and out
+args = commandArgs(trailingOnly=TRUE)
+json_path <- args[1]
+outpath <- args[2]
 
 # Give the input file name to the function.
 result <- fromJSON(file = json_path)
@@ -43,11 +27,10 @@ colnames(grid.df) <- c("dN","dS","posterior")
 
 #as factor 
 grid.factor <- grid.df
-grid.factor$dN<- as.factor(round(grid.factor$dN,digits = 2))
-grid.factor$dS <- as.factor(round(grid.factor$dS,digits = 2))
-grid.factor$log <- log(grid.factor$posterior) # plot log transformed posterior probability
+grid.factor$dN<- as.factor(round(grid.factor$dN,digits = 3))
+grid.factor$dS <- as.factor(round(grid.factor$dS,digits = 3))
+grid.factor$log <- log(grid.factor$posterior,10) # plot log transformed posterior probability
 grid.df$name <- name
-
 
 # Heatmap 
 png(file=outpath,width=800, height=700)
@@ -80,3 +63,14 @@ max.output.file <- file("./max.min.pearson_cor.csv", "a")
 cat(max_min, file=max.output.file, append=TRUE, sep = "\n")
 close(max.output.file)
 """
+                     
+                     
+                     
+                     
+### INFO
+# Read jason file and save jpg of heatmap of posterior probability with set range
+# old_ncbi: midpoint = -5.175, limits=c(-9.59, -0.76))
+# lin(april): midpoint = -2.12, limits=c(-3.86, -0.37))
+# ncbi(april): midpoint =-2.06, limits=c(-3.80, -0.31))
+                     
+                     
