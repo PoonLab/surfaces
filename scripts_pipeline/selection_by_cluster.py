5918ebde0b1ebb4b7406d654124a738a33f66b75
horaciobam <laura.baenam@gmail.com>
2024-04-18 09:36:04 -0400

    Working on including prunning on selection

--- scripts_pipeline/selection_by_cluster.py
+++ scripts_pipeline/selection_by_cluster.py
@@ -1,11 +1,15 @@
-"""
-Create multiple fasta, one for each cluster of proteins
-"""
 import csv
 import argparse
 import sys
 import subprocess
-from Bio import SeqIO
+from Bio import SeqIO, Phylo
+from io import StringIO
+
+description = """\
+Create separate fasta file based on cluster classification.
+Align, create phylogeny, and measure selection with FUBAR. 
+Prune tree to a given length when specified. 
+"""
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -22,16 +26,71 @@ def parse_args():
         '--label', type=str, default='cluster', help = 'label for CDSs files'
     )
     parser.add_argument(
-        '--n_prots', type=int, default=10, 
-        help = 'Maximum numer of clusters allowed'
+        '--n_prots', type=int, default=False, 
+        help = 'Maximum number of clusters allowed'
     )
     parser.add_argument(
         '--run_sel', action='store_true',
         help = 'Run selection pipeline for all clustered sequences'
     )
+    parser.add_argument(
+        '--pn',  type=str, default=False, 
+        help = 'Prune tree'
+    )
 
     return parser.parse_args()
 
+def run_prunetree(phy, aln):
+    """
+    Function from PoonLab/Bioplus/prunetree.py
+    Progressively remove the shortest terminal branches in the tree until
+    we reach a target tree length.
+    :param phy:  Bio.Phylo object
+    :param aln:  Sequence alignment
+    """
+    pruned = subprocess.run['python3', 'prunetree.py', phy, '--mode treelen', '-t 0.5', f'--seq {aln}', f'--outfile {aln}.pruned']
+    return pruned
+
+def prune_length(phy, target):
+    """
+    From PoonLab/Bioplus/prunetree.py
+    Progressively remove the shortest terminal branches in the tree until
+    we reach a target tree length.
+    :param phy:  Bio.Phylo object
+    :param target:  float, target tree length to prune to
+    :return:  Bio.Phylo object
+    """
+    tlen = phy.total_branch_length()
+    if target >= tlen:
+        sys.stderr.write(f"prune_length: requirement already met "
+                         f"({tlen}<={target}")
+    tips = phy.get_terminals()
+    while tlen > target:
+        # we have to re-sort every time because removing a branch
+        # will lengthen another branch
+        tips = sorted(tips, key=lambda x: x.branch_length)
+        _ = phy.prune(tips[0])
+        tlen -= tips[0].branch_length
+        tips = tips[1:]  # update list
+    return phy
+
+
+def run_fasttree(aln):
+    # Create a tree from cleaned a nucleotide alignment
+    tree = subprocess.run(['fasttree', '-nt'], stdout=subprocess.PIPE, 
+                            stderr=subprocess.DEVNULL, stdin=aln.stdout)
+    # t = open(tree_file, 'w')
+    # t.write(tree.stdout.decode("utf-8"))
+    # t.close()
+    return(tree)
+    
+def run_mafft(alignment):
+    # Align nucleotide sequences
+    stdout = subprocess.Popen(['mafft', '--quiet', alignment], stdout=subprocess.PIPE)
+    # stdout = stdout.decode('utf-8')
+    return(stdout)
+    # cmd = f"mafft --quiet {alignment} > {alignment}.mafft"
+    # subprocess.call(cmd, shell=True)
 
 def run_selection_pipeline(alignment, cleaned_file, tree_file, out_name):
     """
@@ -40,21 +99,11 @@ def run_selection_pipeline(alignment, cleaned_file, tree_file, out_name):
     :param tree_file: str, name of file for CDS tree
     :out_name: str, name and location for FUBAR json file 
     """
-    # Align nucleotide sequences
-    cmd = f"mafft --quiet {alignment} > {alignment}.mafft"
-    subprocess.call(cmd, shell=True)
 
     # Clean stop codons from alignment
     subprocess.run(['hyphy', 'cln', 'Universal', f"{alignment}.mafft", 'No/Yes', cleaned_file],
     stdout=subprocess.DEVNULL)
 
-    # Create a tree from cleaned CDS alignment
-    tree = subprocess.run(['fasttree', '-nt', cleaned_file], stdout=subprocess.PIPE, 
-    stderr=subprocess.DEVNULL)
-    t = open(tree_file, 'w')
-    t.write(tree.stdout.decode("utf-8"))
-    t.close()
-    
     # Run FUBAR
     print(f">>> Running FUBAR for: {out_name}")
     p = subprocess.run(['hyphy', 'fubar', cleaned_file, tree_file, '--output', out_name],
@@ -73,23 +122,48 @@ if __name__=="__main__":
         if cluster not in clus_seqs:
             clus_seqs[cluster] = []
         clus_seqs[cluster].append(label)
+    
+    n_prots = args.n_prots if args.n_prots else len(clus_seqs)
 
     # Divide CDSs into different files based on clustering
     for cluster in clus_seqs:
-        if int(cluster) > args.n_prots:
+        if int(cluster) > n_prots:
             continue
 
         print(f"\nProcesing cluster: {cluster}")
-        f_name = f"{args.label}_{cluster}"
-        file = open(f"{f_name}.fa", "w")
+        af_name = f"{args.label}_{cluster}.fa"
+        file = open(af_name, "w")
         for name in clus_seqs[cluster]:
             file.write(f'>{name}\n{seqs[name]}\n')
         file.close()
 
-        # Measure selection with FUBAR
-        if args.run_sel:
-            run_selection_pipeline( f"{f_name}.fa",
-                                    f"{f_name}.cleaned.fa",
-                                    f"{f_name}.cleaned.tree", 
-                                    f"{f_name}.FUBAR.json")
+        # Align sequences
+        align = run_mafft(af_name)
+
+        # Build tree
+        tree = run_fasttree(align)
+
+        handle = StringIO(tree.stdout.decode("utf-8"))
+        phy = Phylo.read(handle, "newick")
+        tlen = phy.total_branch_length()
+        print(f"Tree length: {phy.total_branch_length()}")
+        
+        if args.pn:
+            target = float(tlen)
+            if target <= tlen:
+                sys.stderr.write(f"Starting tree length: {tlen}\n")
+                sys.exit()
+            pruned = prune_length(phy, target=target)
+            
+        
+        
+        sys.exit()
+        
+
+        # # Measure selection with FUBAR
+        # if args.run_sel:
+        #     run_selection_pipeline( f"{f_name}.fa",
+        #                             f"{f_name}.cleaned.fa",
+        #                             f"{f_name}.cleaned.tree", 
+        #                             f"{f_name}.FUBAR.json")
 
