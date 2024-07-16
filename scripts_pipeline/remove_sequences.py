5da60c18910592d16cbff6103d084929d23c66bc
ArtPoon <artpoon@gmail.com>
2024-07-16 10:28:39 -0400

    cleaned up remove_sequences

--- scripts_pipeline/remove_sequences.py
+++ scripts_pipeline/remove_sequences.py
@@ -1,17 +1,32 @@
-#located in  
-#!/usr/bin/env python3
+description = """
+Remove any sequences from target FASTA that are not in reference FASTA.
+This is used after manually removing problematic sequences from a 
+FASTA file, and when there is a second file with the same headers.
+"""
 
 from Bio import SeqIO
 import sys
+import argparse
 
-ffile = SeqIO.parse(sys.argv[1], "fasta")
-header_set = set(line.strip() for line in open(sys.argv[2]))
 
-for seq_record in ffile:
-    try:
-        header_set.remove(seq_record.name)
-    except KeyError:
-        print(seq_record.format("fasta"))
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument("reffile", type=argparse.FileType('r'), 
+                    help="Reference FASTA file with headers manually removed.")
+parser.add_argument("target", type=argparse.FileType('r'),
+                    help="Target FASTA file for modification.")
+parser.add_argument("-o", "--outfile", type=argparse.FileType('w'), default=sys.stdout,
+                    help="Output file, defaults to stdout.")
+args = parser.parse_args()
+
+reffile = SeqIO.parse(args.reffile, "fasta")
+headers = set([record.description for record in reffile])
+missing = []
+for record in SeqIO.parse(args.target, "fasta"):
+    if record.description in headers:
+        args.outfile.write(record.format("fasta"))
         continue
-if len(header_set) != 0:
-    print(len(header_set),'of the headers from list were not identified in the input fasta file.', file=sys.stderr)
+    missing.append(record.description)
+
+if missing:
+    sys.stderr.write(f"Skipped {len(missing)} records from target file.\n")
+
