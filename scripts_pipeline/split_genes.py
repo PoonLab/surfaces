a0d5e5d6e2428c4d690a892b64cbe4dbee0a4cc9
ArtPoon <artpoon@gmail.com>
2024-07-22 17:10:36 -0400

    added split genes scrsipt

---
+++ scripts_pipeline/split_genes.py
@@ -0,0 +1,38 @@
+from Bio import SeqIO
+import sys
+import argparse
+import re
+
+description = """
+Split a FASTA file containing two sets of non-homologous gene sequences.
+For instance, one of the segments of the influenza A virus genome encodes
+M1 and M2 genes, where M2 is produced by splicing a 5' portion of M1 with 
+downstream sequence.  It can be differentiated from M1 by the location 
+field.
+"""
+pat = re.compile("[<>]?([0-9]+)_[<>]?([0-9]+)")
+
+parser = argparse.ArgumentParser(description = "Split FASTA containing M1 and M2 genes "
+                                 "into separate FASTA files")
+parser.add_argument("input", type=argparse.FileType('r'),
+                    help="input FASTA file")
+parser.add_argument("out1", type=argparse.FileType('w'),
+                    help="path to write FASTA of first sequences")
+parser.add_argument("out2", type=argparse.FileType('w'),
+                    help="path to write FASTA of second sequences")
+parser.add_argument("--maxlen", type=int, default=None,
+                    help="optional, partition sequences by length")
+args = parser.parse_args()
+
+for record in SeqIO.parse(args.input, 'fasta'):
+    loc = record.description.split('-')[-1]
+    check = '.' in loc
+    if args.maxlen:
+        matches = pat.findall(loc)
+        if not matches:
+            sys.stderr.write(f"ERROR: Failed to extract coordinates from loc field {loc}\n")
+            sys.exit()
+        left, right = matches[0]
+        check = (int(right)-int(left)) > args.maxlen
+    
+    SeqIO.write(record, args.out1 if check else args.out2, 'fasta')
