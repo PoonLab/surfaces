6ec56f76370fedadd8ecd23cd0effddf366c50c3
ArtPoon <artpoon@gmail.com>
2025-06-25 15:38:11 -0400

    new fingerprint analysis

--- scripts/get_fingerprints.R
+++ scripts/get_fingerprints.R
@@ -1,3 +1,5 @@
+setwd("~/git/surfaces/data")
+
 breaks <- (50*(0:19)^5)/19^5  # from Murrell et al. 2016
 
 # generate fingerprints from FUBAR results and export as JSON
@@ -27,11 +29,11 @@ parse.json <- function(f) {
   values <- json$grid[1:20, 2]
   
   # empirical distribution
-  mle <- json$MLE$content[["0"]]
-  emp.grid <- fprint(alpha=mle[,1], beta=mle[,2])
+  #mle <- json$MLE$content[["0"]]
+  #emp.grid <- fprint(alpha=mle[,1], beta=mle[,2])
   
-  list(virus=virus, protein=protein, step=step, values=values, grid=grid, 
-       emp.grid=emp.grid)
+  list(virus=virus, protein=protein, step=step, values=values, grid=grid) 
+       #emp.grid=emp.grid)
 }
 
 # default Dirichlet parameter at 0.5
@@ -45,18 +47,20 @@ files.2 <- Sys.glob("6_fubar/concentration_1/*.fubar.json")
 grids.2 <- lapply(files.2, function(f) parse.json(f))
 
 # visualize results
-start <- 51
-end <- start + 24
-png(paste("fpt", start, "-", end, "_c0.5.png", sep=''), 
-    width=6*300, height=6*300, res=300)
-par(mfrow=c(5, 5), mar=c(0,0,0,0))
-for (i in start:end) {
-  image(grids[[i]]$emp.grid, xaxt='n', yaxt='n')
-  abline(a=0, b=1, col=rgb(0,0,0,0.2))
-  text(x=0.5, y=0.95, cex=0.8,
-       label=paste(grids[[i]]$virus, grids[[i]]$protein, sep=" "))
+for (start in seq(1, length(grids), 25)) {
+  end <- min(start + 24, length(grids))
+  png(paste("fpt", start, "-", end, "_c0.5.png", sep=''), 
+      width=6*300, height=6*300, res=300)
+  par(mfrow=c(5, 5), mar=c(0,0,0,0))
+  for (i in start:end) {
+    image(grids[[i]]$grid, xaxt='n', yaxt='n')
+    abline(a=0, b=1, col=rgb(0,0,0,0.2))
+    text(x=0.5, y=0.95, cex=0.8,
+         label=paste(grids[[i]]$virus, grids[[i]]$protein, sep=" "))
+  }
+  dev.off()  
 }
-dev.off()
+
 
 png(paste("fpt", start, "-", end, "_c1.0.png", sep=''), 
     width=6*300, height=6*300, res=300)
@@ -102,18 +106,56 @@ ix <- lower.tri(wmat, diag=FALSE)
 wmat[ix] <- t(wmat)[ix]  
 rownames(wmat) <- names(wpps)
 colnames(wmat) <- names(wpps)  
-write.csv(wmat, file="wdist.csv", quote=F)
+write.csv(wmat, file="wdist-revised.csv", quote=F)
+
+wmat <- read.csv("wdist-revised.csv", row.names=1)
 
 wdist <- as.dist(wmat)
 
 mds <- cmdscale(wdist, k=2)
-par(mfrow=c(1,1))
+par(mfrow=c(1,1), mar=c(0,0,0,0))
 plot(mds[,1:2], type='n')
 text(mds[,1], mds[,2], labels=names(wpps), cex=0.5)
 
+require(uwot)
+u <- uwot::umap(wdist, n_components=2)
+
+pdf("umap.pdf", width=8, height=8)
+par(mar=c(0,0,0,0))
+plot(u, type='n')
+labels <- gsub("nonstructural|protein", "", row.names(u))
+text(u[,1], u[,2], labels=labels, cex=0.5)
+dev.off()
 
 hc <- hclust(wdist)
 pdf("hclust.pdf", width=15, height=5)
 par(mar=c(0,0,0,0))
 plot(hc, cex=0.5, main=NA)
 dev.off()
+
+
+# generate a graph
+hist(wmat[upper.tri(wmat)], breaks=100, xlim=c(0, 1))
+rug(wmat[upper.tri(wmat)])
+table(wmat[upper.tri(wmat)] < 1.0)
+
+cutoff <- max(apply(wmat, 1, function(x) min(x[x>0])))
+n <- nrow(wmat)
+nm <- gsub("protein|non.*structural", "", names(wmat))
+k <- 2
+adj <- matrix(0, nrow=n, ncol=n, dimnames=list(nm, nm))
+for (i in 1:n) {
+  row <- as.numeric(wmat[i,])
+  ranks <- order(row)
+  nn <- ranks[-(ranks==i)][1:k]
+  nn <- nn[row[nn] <= cutoff]
+  adj[i, nn] <- 1
+}
+
+require(igraph)
+g <- graph_from_adjacency_matrix(adj, mode="max")
+pdf("graph.pdf", width=8, height=8)
+plot(g, layout=layout_with_gem, vertex.label.cex=0.6, vertex.size=1.5)
+dev.off()
+
+write.csv(adj, file="adjmat.csv", quote=FALSE)
